1bd179aa64af94c4bc68e58ea30956f945a31a68
Jefferson Boldrin Cardozo <jefferson.boldrin@gmail.com>
2017-10-31 02:55:37 -0200

    Add demo scenes

---
+++ Assets/Demo/CharacterCreation/downArrowSimpleAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class downArrowSimpleAnimation : MonoBehaviour {
+      
+    public float magnitude = 3f;
+    public float speed = 3f;
+
+    float x0, y0, t0;
+    RectTransform rt;
+
+    void Start() {
+        rt = (RectTransform)transform;
+        x0 = rt.anchoredPosition.x;
+        y0 = rt.anchoredPosition.y;
+    }
+
+    void OnEnable() {
+        t0 = Time.time;
+    }
+
+	void Update () {
+        rt.anchoredPosition = new Vector3(x0, y0 + Mathf.Cos((Time.time- t0) * speed) * magnitude, 0);
+	}
+}
---
+++ Assets/Demo/MainMenu/mainMenuScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.SceneManagement;
+
+public class mainMenuScript : Script {
+
+	public override void StartNodeSwitch(int startingNode) {
+        switch (startingNode) {
+            case 0:
+                StartText("Select a scene", 1);
+                break;
+            case 1:
+                AddDecisionButton(0, "Character Creation Demo");
+                AddDecisionButton(1, "NPC Chat Demo (Tutorial)");
+                StartDecision();
+                break;
+        }
+    }
+
+    public override void ButtonActionSwitch(int buttonID) {
+        switch (buttonID) {
+            case 0:
+                SceneManager.LoadScene("CharacterCreation");
+                break;
+            case 1:
+                SceneManager.LoadScene("NpcChat");
+                break;
+        }
+    }
+}
